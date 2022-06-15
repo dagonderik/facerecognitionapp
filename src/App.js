bdec12a1c1468e502cd892564e32d07e6a4a1701
dagonderik <dagonderik@gmail.com>
2022-06-15 12:29:33 -0300

    adding API integration

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
 import Navigation from './components/navigation/Navigation.js';
 import Logo from './components/logo/Logo.js';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition.js';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js';
 import Rank from './components/Rank/Rank.js';
-import React from 'react';
+import React,{Component} from 'react';
 import Particles from "react-tsparticles";
 import { loadFull } from "tsparticles";
+import Clarifai from 'clarifai';
 
+//API key
+const app = new Clarifai.App({
+  apiKey: '53eeac0f387043d28b33eac55b57b453'
+})
 
+//configuration of the particles
 const particlesOptions = {
   fpsLimit:120,
   interactivity: {
@@ -69,36 +76,62 @@ const particlesOptions = {
   detectRetina: true,
 }
 
-function App() {
+const particlesInit = async (main) => {
 
-  const particlesInit = async (main) => {
-    console.log(main);
+  // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+  // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+  // starting from v2 you can add only the features you need reducing the bundle size
+  await loadFull(main);
+};
 
-    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
-    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-    // starting from v2 you can add only the features you need reducing the bundle size
-    await loadFull(main);
-  };
+const particlesLoaded = (container) => {
+};
 
-  const particlesLoaded = (container) => {
-    console.log(container);
-  };
+class App extends Component {
 
-  return (
-    <div className="App">
-      <Particles
-        id="tsparticles"
-        init={particlesInit}
-        loaded={particlesLoaded}
-        options={particlesOptions}
-      />
-      <Navigation/>
-      <Logo/>
-      <Rank/>
-      <ImageLinkForm/>
-      {/* <FaceRecognition/> */}
-    </div>
-  );
+  constructor()  {
+    super();
+    this.state = {
+      input: '',
+      imgUrl: ''
+    }
+  }
+  
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  } 
+
+  onButtonSubmit = () => {
+    this.setState({imgUrl: this.state.input})
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
+      function(response) {
+        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+      },
+      function(err) {
+
+      }
+    );
+  }
+
+  render (){
+    return (
+      <div className="App">
+       <Particles
+           id="tsparticles"
+           init={particlesInit}
+           loaded={particlesLoaded}
+           options={particlesOptions}
+       />
+        <Navigation/>
+        <Logo/>
+        <Rank/>
+        <ImageLinkForm 
+        onInputChange={this.onInputChange}
+        onButtonSubmit={this.onButtonSubmit}/>
+        <FaceRecognition imageUrl={this.state.imgUrl}/>
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -3,7 +3,7 @@ import './ImageLinkForm.css';
 import Tilt from 'react-parallax-tilt';
 
 
-const ImageLinkForm = () =>{
+const ImageLinkForm = ({ onInputChange, onButtonSubmit }) =>{
     return (
        <div>
            <p className='f3'>
@@ -13,8 +13,8 @@ const ImageLinkForm = () =>{
                 <Tilt className='form Tilt outline center pa4 br3 shadow-5' 
                 tiltMaxAngleX={0}
                 tiltMaxAngleY={0}>
-                    <input className='f4 pa2 w-70 center' type='tex'/>
-                    <button className='w-30 grow f4 link ph3 pv2 dib white bg-dark-green'>Detect</button>
+                    <input className='f4 pa2 w-70 center' type='tex' onChange={onInputChange}/>
+                    <button className='w-30 grow f4 link ph3 pv2 dib white bg-dark-green' onClick={onButtonSubmit}>Detect</button>
                 </Tilt>
             </div> 
        </div>
